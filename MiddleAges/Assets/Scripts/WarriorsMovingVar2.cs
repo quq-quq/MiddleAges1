dc6236debb5e3cbdc550807bb8aec58abb817895
quq-quq <kubrakfedor@gmail.com>
2022-11-30 00:29:15 +0300

    че то типо фигня нихера не сделал

--- MiddleAges/Assets/Scripts/WarriorsMovingVar2.cs
+++ MiddleAges/Assets/Scripts/WarriorsMovingVar2.cs
@@ -10,7 +10,16 @@ public class WarriorsMovingVar2 : MonoBehaviour
     void Start()
     {
         Vector3 warPos;
-        int stepx = -len, stepz = -len, lim = transform.childCount/16;
+        int stepx = -len, stepz = -len, lim = 0, lim1 = transform.childCount, n = 0;
+        while (lim1 > 0)
+        {
+            lim1 -= 8 * (n + 1);
+            n += 1;
+            lim += 1;
+        }
+
+
+
         Debug.Log(lim);
 
         for(int i = 0; i < transform.childCount; i++)
@@ -20,8 +29,8 @@ public class WarriorsMovingVar2 : MonoBehaviour
             if(warPos != new Vector3 (0, 0, 0)) 
                 transform.GetChild(i).gameObject.transform.localPosition = warPos;
 
-            //подсчет позиций:
-            if (stepx < len)
+
+            if (stepx < len && stepz < len)
                 stepx += len;
             else if (stepx == len && stepz < len)
             {
@@ -29,14 +38,24 @@ public class WarriorsMovingVar2 : MonoBehaviour
                 stepx = -len;
             }
 
-            if(stepx == 0 && stepz == 0)
-                stepx += len;
+
+            //подсчет позиций:
+            //if (stepx < len)
+            //    stepx += len;
+            //else if (stepx == len && stepz < len)
+            //{
+            //    stepz += len;
+            //    stepx = -len;
+            //}
+
+            //if(stepx == 0 && stepz == 0)
+            //    stepx += len;
 
             //if( i % 8 == 0)
             //    len = 
 
         }
-    }
+        }
 
     // Update is called once per frame
     void Update()
