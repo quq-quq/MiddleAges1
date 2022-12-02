66c79648b93736a1698ab9e20562488179bcb676
quq-quq <kubrakfedor@gmail.com>
2022-12-03 01:09:31 +0300

    pre Dash

--- MiddleAges/Assets/Scripts/Entities/Player.cs
+++ MiddleAges/Assets/Scripts/Entities/Player.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class Player : EntityBehaviour
 { 
+
     public static Transform playerTransform;
     public static Vector3 movementVector;
+    public Transform dashPos;
+    public int speedOfDash;
+
+    private bool isDash;
+    private float disToMyPlace;
 
     private void Awake()
     {
@@ -21,7 +29,35 @@ public class Player : EntityBehaviour
 
     private void FixedUpdate()
     {
-        movementVector = playerTransform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), -2, Input.GetAxis("Vertical")));
-        controller.Move(movementVector * speed * Time.deltaTime);
+        if ((Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E)) && !isDash)
+        {
+            StartDash();
+        }
+        else if (!isDash)
+        {
+            movementVector = playerTransform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), -2, Input.GetAxis("Vertical")));
+            controller.Move(movementVector * speed * Time.deltaTime);
+            dashPos.localEulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg, 0);
+        }
+    }
+
+    private void StartDash()
+    {
+        disToMyPlace = Vector3.Distance(transform.position, dashPos.GetChild(0).position);
+        while (disToMyPlace >= 1f)
+        {
+            movementVector = (dashPos.GetChild(0).position - transform.position).normalized;
+            movementVector = new Vector3(movementVector.x, -2f, movementVector.z);
+            controller.Move(movementVector * speedOfDash * Time.deltaTime);
+
+        }
+        StartCoroutine("ToDash");
+    }
+
+    IEnumerator ToDash()
+    {
+        isDash = true;
+        yield return new WaitForSeconds(2f);
+        isDash = false;
     }
 }
